bfcbcd92b5bba2e53dc163a3d7ec5a5c0b5d2d1b
Christopher Bura <christopher.bura@gmail.com>
2013-12-22 00:57:27 -0500

    Refactor out common table parts, add generic foreignkey code

--- PythonCAD/Generic/Kernel/Db/schema.py
+++ PythonCAD/Generic/Kernel/Db/schema.py
@@ -1,21 +1,58 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String, Boolean, Float, ForeignKey
-from sqlalchemy.orm import mapper, relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey, and_
+from sqlalchemy.orm import relationship, foreign, remote, backref
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import event
 
 from Kernel import models
 
-Base = declarative_base()
-
-class Layer(Base):
-    __tablename__ = 'layer'
+@as_declarative()
+class Base(object):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    visible = Column(Boolean, default=True)
 
 
-class Point(Base):
-    __tablename__ = 'point'
-    id = Column(Integer, primary_key=True)
+class Entity(Base):
+    layer_id = Column(Integer, ForeignKey('layer.id'))
+
+    layer = relationship('Layer', foreign_keys=layer_id)
+
+    # Generic fk related columns
+    content_type = Column(String)
+    object_id = Column(Integer)
+
+    @property
+    def content_object(self):
+        return getattr(self, 'parent_{0}'.format(self.content_type))
+
+
+class EntityType(object):
+    pass
+
+
+# From sqlalchemy example files examples/generic_associations/generic_fk.py
+@event.listens_for(EntityType, 'mapper_configured', propagate=True)
+def setup_listener(mapper, class_):
+    name = class_.__name__
+    content_type = name.lower()
+    # Reverse relationship (ie. Segment.entities)
+    class_.entities = relationship(Entity,
+        primaryjoin = and_(
+            class_.id == foreign(remote(Entity.object_id)),
+            Entity.content_type == content_type
+        ),
+        backref = backref(
+            'parent_{0}'.format(content_type),
+            primaryjoin=remote(class_.id) == foreign(Entity.object_id)
+        )
+    )
+    @event.listens_for(class_.entities, 'append')
+    def append_entity(target, value, initiator):
+        value.content_type = content_type
+
+
+class Point(EntityType, Base):
     x = Column(Float)
     y = Column(Float)
 
@@ -23,9 +60,7 @@ class Point(Base):
         return '<Point ({self.x}, {self.y})>'.format(self=self)
 
 
-class Segment(Base):
-    __tablename__ = 'segment'
-    id = Column(Integer, primary_key=True)
+class Segment(EntityType, Base):
     point1_id = Column(Integer, ForeignKey('point.id'))
     point2_id = Column(Integer, ForeignKey('point.id'))
 
@@ -33,18 +68,16 @@ class Segment(Base):
     point2 = relationship('Point', foreign_keys=point2_id)
 
 
-class Circle(Base):
-    __tablename__ = 'circle'
-    id = Column(Integer, primary_key=True)
+class Circle(EntityType, Base):
+    # TODO: Convert to two points
     point_id = Column(Integer, ForeignKey('point.id'))
     radius = Column(Float)
 
     point = relationship('Point')
 
 
-class Ellipse(Base):
-    __tablename__ = 'ellipse'
-    id = Column(Integer, primary_key=True)
+class Ellipse(EntityType, Base):
+    # TODO: Conver to three points
     point_id = Column(Integer, ForeignKey('point.id'))
     radius_x = Column(Float)
     radius_y = Column(Float)
@@ -52,12 +85,15 @@ class Ellipse(Base):
     point = relationship('Point')
 
 
+class Layer(Base):
+    name = Column(String(50))
+    visible = Column(Boolean, default=True)
+
+
 class Setting(Base):
     """
     Setting represents individual document settings such as active_layer
 
     """
-    __tablename__ = 'settings'
-    id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     value = Column(String(50))
