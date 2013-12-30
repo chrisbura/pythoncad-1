6ae4fbce937d75ccb618f77aa42ae01d6451df37
Christopher Bura <christopher.bura@gmail.com>
2013-12-29 21:31:10 -0500

    Put proper license in mainwindow.py

--- pythoncad/interface/mainwindow.py
+++ pythoncad/interface/mainwindow.py
@@ -1,25 +1,22 @@
-############################################################################
 #
-#  Copyright (C) 2004-2005 Trolltech AS. All rights reserved.
+# Copyright (c) 2010 Matteo Boscolo
 #
-#  This file is part of the example classes of the Qt Toolkit.
+# This file is part of PythonCAD.
 #
-#  This file may be used under the terms of the GNU General Public
-#  License version 2.0 as published by the Free Software Foundation
-#  and appearing in the file LICENSE.GPL included in the packaging of
-#  this file.  Please review the following information to ensure GNU
-#  General Public Licensing requirements will be met:
-#  http://www.trolltech.com/products/qt/opensource.html
+# PythonCAD is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#  If you are unsure which license is appropriate for your use, please
-#  review the following information:
-#  http://www.trolltech.com/products/qt/licensing.html or contact the
-#  sales department at sales@trolltech.com.
+# PythonCAD is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# You should have received a copy of the GNU General Public License
+# along with PythonCAD; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-############################################################################
 
 # This is only needed for Python v2 but is harmless for Python v3.
 
